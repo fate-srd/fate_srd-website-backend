0049fdb00b9f893fb43e6aa83f6721cd60d6ba56
Randy Oest <oest@amazingrando.com>
2025-09-18 13:12:36 -0400

    starting upgrade process

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -788,7 +788,6 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  *
  * Keep this code block at the end of this file to take full effect.
  */
-#
 
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
@@ -799,4 +798,4 @@ if (file_exists($app_root . '/' . $site_path . '/settings.live.php')) {
 }
 
 $settings['config_sync_directory'] = 'config/sync';
-$config['system.logging']['error_level']='verbose';
+$config['system.logging']['error_level'] = 'verbose';
