1e93685963cdcb1e77da80297dc89b746bea1ff0
Randy Oest <oest@amazingrando.com>
2025-10-03 16:05:10 -0400

    small drupal 11 update

--- fix_profile.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=database;dbname=drupal9;charset=utf8mb4","drupal9","drupal9",[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
-$data = $pdo->query("SELECT data FROM config WHERE name='core.extension'")->fetchColumn();
-$arr = @unserialize($data);
-if ($arr === false || !is_array($arr)) { fwrite(STDERR, "Could not unserialize core.extension\n"); exit(1); }
-$arr['profile'] = 'standard';
-$pdo->prepare("UPDATE config SET data=? WHERE name='core.extension'")->execute([serialize($arr)]);
-echo "profile set to standard\n";
